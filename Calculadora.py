1035116002433c818a2bf1b65950dcf2c44b44d9
Aluno Lab 135 <lab135@lab.senai>
2023-08-25 20:27:10 -0300

    novamente o codigo

--- Calculadora.py
+++ Calculadora.py
@@ -2,5 +2,7 @@ n1=int(input("digite o primeiro numero : "))
 n2=int(input("digite o segundo numero : "))
 soma=n1+n2
 subtracao=n1-n2
+multiplicacao=n1*n2
 print("a soma é",soma)
 print("a subtraçao dos numeros é",subtracao)
+print("a multiplicaçao dos numeros é :",multiplicacao)
