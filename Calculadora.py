9ad36672272cb20bcd232cd9c4e5b38db85861f5
Aluno Lab 135 <lab135@lab.senai>
2023-08-25 20:11:44 -0300

    calculadora com subtraçao

--- Calculadora.py
+++ Calculadora.py
@@ -1,4 +1,6 @@
 n1=int(input("digite o primeiro numero : "))
 n2=int(input("digite o segundo numero : "))
 soma=n1+n2
+subtracao=n1-n2
 print("a soma é",soma)
+print("a subtraçao dos numeros é",subtracao)
