93e598baa031560a8f42b0524e501f7d51937bec
Aluno Lab 135 <lab135@lab.senai>
2023-08-25 21:47:42 -0300

    calcula com divisões

--- Calculadora.py
+++ Calculadora.py
@@ -3,6 +3,11 @@ n2=int(input("digite o segundo numero : "))
 soma=n1+n2
 subtracao=n1-n2
 multiplicacao=n1*n2
+if n2!=0:
+   divisao=n1/n2
+   print("a divisão dos numeros é :",divisao)
+else:
+    print("impossivel")
 print("a soma é",soma)
 print("a subtraçao dos numeros é",subtracao)
 print("a multiplicaçao dos numeros é :",multiplicacao)
