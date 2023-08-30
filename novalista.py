6e7c0ddf26cf302fd4ff73ebfa40799cf55deed6
Aluno Lab 135 <lab135@lab.senai>
2023-08-29 21:56:18 -0300

    exibir-sequencias

---
+++ novalista.py
@@ -0,0 +1,12 @@
+num1 = 0
+# Versão 1 Funcionalidade esperada: seu programa deverá mostrar a sequência 0,25,50,75,100,125,150,175,200 (utilize loops)
+while (num1 <= 200):
+    print(num1)
+    num1 = num1 + 25
+
+print("")
+#Funcionalidade esperada: seu programa deverá mostrar a sequência 200,100,50,25 (utilize loops)
+num1 = 200
+while (num1 >= 25):
+    print(num1)
+    num1 = num1 / 2
